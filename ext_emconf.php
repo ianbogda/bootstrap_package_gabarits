5ce9bc8c0ccb7b01b7bc2355c9bdd78faa5bd029
ianbogda <ianbogda@gmail.com>
2015-06-26 10:56:51 +0200

    + add typo3 templates conf files

---
+++ ext_emconf.php
@@ -0,0 +1,22 @@
+<?php
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Bootstrap Package Gabarits',
+    'description' => 'Extends bootstrap_package for templates',
+    'category' => 'templates',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '6.2.13-7.99.99',
+            'bootstrap_package' => '6.2.12-7.99.99',
+        ),
+        'conflicts' => array(),
+    ),
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Yann Bogdanovic',
+    'author_email' => 'ianbogda@gmail.com',
+    'author_company' => 'private',
+    'version' => '6.2.12',
+);
+?>
---
+++ ext_localconf.php
@@ -0,0 +1,12 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die('Access denied.');
+}
+
+// Add BackendLayouts BackendLayouts for the BackendLayout DataProvider
+if (!$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]['disablePageTsBackendLayouts']) {
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/Mod/web_layout.txt">');
+} else {
+        !$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['bootstrap_package']['disablePageTsBackendLayouts'] = 0;
+}
+?>
