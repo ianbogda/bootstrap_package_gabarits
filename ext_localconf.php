6ba1bf140f0963ca9332259632cb9600272d2128
ianbogda <ianbogda@gmail.com>
2017-01-24 11:42:56 +0100

    - fix

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,14 +3,14 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/Mod/web_layout.txt">');
 // Add BackendLayouts BackendLayouts for the BackendLayout DataProvider
-/*
 if (!$GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]['disablePageTsBackendLayouts']) {
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/Mod/web_layout.txt">');
-        $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['bootstrapPackage']['disablePageTsBackendLayouts'] = 1;
-} else {
-        $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['bootstrapPackage']['disablePageTsBackendLayouts'] = 0;
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/Mod/WebLayout/BackendLayouts.txt">');
 }
-*/
-?>
+
+/***************
+ * Reset extConf array to avoid errors
+ */
+if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY])) {
+    $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY] = serialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
+}
